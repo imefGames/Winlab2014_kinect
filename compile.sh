56b09ef7b1ba175433a8e1e917fb9c60d54fbd5b
imefGames <michel.pierre.35@gmail.com>
2014-08-11 18:19:36 -0400

    New bash file

--- compile.sh
+++ compile.sh
@@ -1,2 +1,16 @@
+//Compiler instructions for one kinect
+gcc calibrateOneKinect.c kinectDetectionUtil.c -o calibrateOne -lm -lfreenect_sync;
+gcc detectOneKinect.c kinectDetectionUtil.c -o detectOne -lm -lfreenect_sync -pthread
+
+//Compiler instructions for two kinects
 gcc calibrate.c kinectDetectionUtil.c -o calibrate -lm -lfreenect_sync;
 gcc detect.c kinectDetectionUtil.c -o detect -lm -lfreenect_sync -pthread;
+
+
+//Compiler instructions for one kinect to 2 IPs
+gcc detectOneKinect2IP.c kinectDetectionUtil.c -o detectOne2IP -lm -lfreenect_sync -pthread
+
+//Compiler instructions for two kinects to IPs
+gcc detect2IP.c kinectDetectionUtil.c -o detect2IP -lm -lfreenect_sync -pthread;
+
+
